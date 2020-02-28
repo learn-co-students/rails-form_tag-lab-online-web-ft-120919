71b56bf7533e3890bdaeff0184add06caf57af14
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-02-28 11:36:45 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,18 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+  end
+
+  def create
+    @student = Student.new(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
   def show
     @student = Student.find(params[:id])
   end
